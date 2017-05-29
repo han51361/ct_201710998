551865892e683ebe2568ee1404551d48e50ce0a1
han51361 <han51361@naver.com>
2017-05-29 17:09:19 +0900

    Add files via upload

---
+++ ct_201710998/201710998-12-2.py
@@ -0,0 +1,51 @@
+﻿import turtle
+wn = turtle.Screen()
+t1 = turtle.Turtle()
+width =wn.window_width()
+print width
+
+x1 = 0.0 -( width - 40)/3
+x2 = 0.0
+x3 =0.0 +( width - 40)/3
+
+def drawSquare(x,y,size):
+    t1.penup()
+    t1.goto(x,y)
+    t1.setheading(0)
+    t1.pendown()
+    t1.pos()
+    t1.write(t1.pos())
+    for i in range(1,5) :
+         t1.fd(size)
+         t1.right(90)
+
+def drawTriangleAt(x,y,size):
+    t1.penup()
+    t1.goto(x,y)
+    t1.setheading(0)
+    t1.pendown()
+    t1.pos()
+    t1.write(t1.pos())
+    for i in range(1,4) :
+            t1.right(120)
+            t1.fd(size)
+ 
+    
+def drawStarAt(x,y,size):
+    t1.penup()
+    t1.goto(x,y)
+    t1.setheading(0)
+    t1.pendown()
+    t1.pos()
+    t1.write(t1.pos())
+    for i in range(1,6) :
+            t1.right(144)
+            t1.fd(size)
+   
+
+drawSquare(x1,100,100)
+drawTriangleAt(x2,100,100)
+drawStarAt(x3,100,100)
+
+win.exitonclick()
+
